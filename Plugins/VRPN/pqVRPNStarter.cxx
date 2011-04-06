02f10fa91f444c04e3a23c4859f04de3a663639b
Alexis YL Chan <alexisc@cs.unc.edu>
2011-04-06 00:22:00 -0400

    clean code.

--- Plugins/VRPN/pqVRPNStarter.cxx
+++ Plugins/VRPN/pqVRPNStarter.cxx
@@ -107,43 +107,29 @@ void pqVRPNStarter::onStartup()
   vtkPVOptions *options = (vtkPVOptions*)pm->GetOptions();
 
   pqApplicationCore* core = pqApplicationCore::instance();
-  
-  //pqMultiView* multiView = qobject_cast<pqMultiView*>(core->manager("MULTIVIEW_MANAGER"));
-  //pqMultiViewFrame* multiViewFrame = multiView->splitWidgetHorizontal(qobject_cast<QWidget*>(this));
-  //core->getObjectBuilder()->createView(QString("RenderView"),pqActiveObjects::instance().activeServer());
 
   if(options->GetUseVRPN())
     {
     // VRPN input events.
     this->VRPNTimer=new QTimer(this);
     this->VRPNTimer->setInterval(4); // in ms
-    // to define: obj and callback()
    
-	/////////////////////////GET VIEWS////////////////////////////
+	/////////////////////////GET VIEW////////////////////////////
 
 	// Get the Server Manager Model so that we can get each view
 	pqServerManagerModel* serverManager = core->getServerManagerModel();
-	//if (serverManager->getNumberOfItems<pqView*> () == 2) //Check that there really are 2 views
-	//{
 		//Get Views
-		pqView* view1 = serverManager->getItemAtIndex<pqView*>(0); // First View
-		//pqView* view2 = serverManager->getItemAtIndex<pqView*>(1); // Second View
+		pqView* view1 = serverManager->getItemAtIndex<pqView*>(0); 
 	
-		//Get View Proxies
+		//Get View Proxy
 		vtkSMRenderViewProxy *proxy1 = 0;
 		proxy1 = vtkSMRenderViewProxy::SafeDownCast( view1->getViewProxy() ); 
-		/*vtkSMRenderViewProxy *proxy2 = 0;
-		proxy2 = vtkSMRenderViewProxy::SafeDownCast( view2->getViewProxy() ); */
       
 		//Get Renderer and Render Window
 		vtkRenderer* renderer1 = proxy1->GetRenderer();
 		vtkRenderWindow* window1 = proxy1->GetRenderWindow();
 
-		////Get Renderer and Render Window
-		//vtkRenderer* renderer2 = proxy2->GetRenderer();
-		//vtkRenderWindow* window2 = proxy2->GetRenderWindow();
-
-	/////////////////////////CREATE FIRST TRACKER////////////////////////////
+	/////////////////////////CREATE  TRACKER////////////////////////////
 
 	//Create connection to VRPN Tracker using vtkInteractionDevice.lib
 	vtkVRPNTrackerCustomSensor* tracker1 = vtkVRPNTrackerCustomSensor::New();
@@ -162,63 +148,29 @@ void pqVRPNStarter::onStartup()
     tracker1->SetTracker2WorldRotation(t2wQuat1);
 
     tracker1->Initialize();
-	///////////////////////////CREATE SECOND TRACKER////////////////////////////
 
-	////Create connection to VRPN Tracker using vtkInteractionDevice.lib
-	//vtkVRPNTrackerCustomSensor* tracker2 = vtkVRPNTrackerCustomSensor::New();
- //   tracker2->SetDeviceName(options->GetVRPNAddress()); 
-	//tracker2->SetSensorIndex(1);//TODO: Fix error handling if there is only  1 sensor?
-
-	////My custom Tracker placement
-	//tracker2->SetTracker2WorldTranslation(-8.68, -5.4, -1.3);
-
- //   // Rotate 90 around x so that tracker is pointing upwards instead of towards view direction.
- //   double t2w2[3][3] = { 1, 0,  0,
- //                        0, 0, -1, 
- //                        0, 1,  0 };
- //   double t2wQuat2[4];
- //   vtkMath::Matrix3x3ToQuaternion(t2w2, t2wQuat2);
- //   tracker2->SetTracker2WorldRotation(t2wQuat2);
-
- //   tracker2->Initialize();
-
-	/////////////////////////CREATE FIRST TRACKER STYLE////////////////////////////
+	/////////////////////////CREATE  TRACKER STYLE////////////////////////////
 
 	//Create device interactor style (defined in vtkInteractionDevice.lib) that determines how the device manipulates camera viewpoint
     vtkVRPNTrackerCustomSensorStyleCamera* trackerStyleCamera1 = vtkVRPNTrackerCustomSensorStyleCamera::New();
     trackerStyleCamera1->SetTracker(tracker1);
     trackerStyleCamera1->SetRenderer(renderer1);
-	///////////////////////////CREATE SECOND TRACKER STYLE////////////////////////////
 
-	////Create device interactor style (defined in vtkInteractionDevice.lib) that determines how the device manipulates camera viewpoint
- //   vtkVRPNTrackerCustomSensorStyleCamera* trackerStyleCamera2 = vtkVRPNTrackerCustomSensorStyleCamera::New();
- //   trackerStyleCamera2->SetTracker(tracker2);
- //   trackerStyleCamera2->SetRenderer(renderer2);
-
-	/////////////////////////INTERACTORS////////////////////////////
+	/////////////////////////INTERACTOR////////////////////////////
 	// Initialize Device Interactor to manage all trackers
     inputInteractor = vtkDeviceInteractor::New();
     inputInteractor->AddInteractionDevice(tracker1);
     inputInteractor->AddDeviceInteractorStyle(trackerStyleCamera1);
-   /* inputInteractor->AddInteractionDevice(tracker2);
-    inputInteractor->AddDeviceInteractorStyle(trackerStyleCamera2);*/
 
 	//Get vtkRenderWindowInteractors
 	vtkRenderWindowInteractor* interactor1 = vtkRenderWindowInteractor::New();
-	/*vtkRenderWindowInteractor* interactor2 = vtkRenderWindowInteractor::New();*/
 
 	//Set the vtkRenderWindowInteractor's style (trackballcamera) and window 
 	vtkInteractorStyleTrackballCamera* interactorStyle1 = vtkInteractorStyleTrackballCamera::New();
     interactor1->SetRenderWindow(window1);
     interactor1->SetInteractorStyle(interactorStyle1);
-	/*vtkInteractorStyleTrackballCamera* interactorStyle2 = vtkInteractorStyleTrackballCamera::New();
-    interactor2->SetRenderWindow(window2);
-    interactor2->SetInteractorStyle(interactorStyle2);
-	*/
 	//Set the View Proxy's vtkRenderWindowInteractor
 	proxy1->GetRenderWindow()->SetInteractor(interactor1);
-	/*proxy2->GetRenderWindow()->SetInteractor(interactor2);
-	*/
 
 	//Cory Quammen's Code
 	const char * spaceNavigatorAddress = "device0@localhost";
@@ -231,7 +183,6 @@ void pqVRPNStarter::onStartup()
     connect(this->VRPNTimer,SIGNAL(timeout()),
 		 this,SLOT(callback()));
     this->VRPNTimer->start();
-	/*}*/
     }
 }
 
@@ -355,7 +306,6 @@ const vrpn_ANALOGCB t)
 				vtkSMPropertyHelper(repProxy,"Position").Set(pos,3);
 				vtkSMPropertyHelper(repProxy,"Orientation").Set(orient,3);
 				repProxy->UpdateVTKObjects();
-				//viewProxy->GetRenderWindow()->Render();
 			  }
 		  }
 		
